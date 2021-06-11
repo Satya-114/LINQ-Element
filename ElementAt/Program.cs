44351e10a22f7aa62bb73e45119e7068429e6707
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 21:06:04 +0530

    Add files via upload

---
+++ ElementAt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_ElementAt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int MethodSyntax = numbers.ElementAt(1);
+            Console.WriteLine(MethodSyntax);
+            Console.ReadLine();
+        }
+    }
+}
