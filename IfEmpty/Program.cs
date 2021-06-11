771103b8c57e5401549872723a0570dff5604e1f
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 22:18:36 +0530

    Add files via upload

---
+++ IfEmpty/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_DefaultIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 10, 20, 30 };
+            IEnumerable<int> result = numbers.DefaultIfEmpty();
+            foreach (int num in result)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
