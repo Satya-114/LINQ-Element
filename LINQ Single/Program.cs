7c77e2760d5db2331717d4d775ef56753ae3d469
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 22:13:20 +0530

    Add files via upload

---
+++ LINQ Single/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_SingleElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sequence contains one element
+            List<int> numbers = new List<int>() { 10 };
+            int number = numbers.Single();
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
